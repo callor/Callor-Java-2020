90a0f975ff73418a891bd05e48c9e2589ae3a759
callor <callor@callor.com>
2019-09-24 17:05:01 +0900

    bankwork2

--- Java_2019_30_BankWork_01/src/com/callor/bank/exec/BankAccEx_01.java
+++ Java_2019_30_BankWork_01/src/com/callor/bank/exec/BankAccEx_01.java
@@ -1,8 +1,10 @@
 package com.callor.bank.exec;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import com.callor.bank.service.BankAccServiceV1;
+import com.callor.bank.util.BankInfo;
 
 public class BankAccEx_01 {
 	
@@ -10,7 +12,17 @@ public class BankAccEx_01 {
 		BankAccServiceV1 bs = new BankAccServiceV1("0001",10000);
 		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println(BankInfo.BOOK.NUM);
+		
+		
 		while(true) {
+		    try {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} catch (InterruptedException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    
 			System.out.println("==============================");
 			System.out.println("대한은행 계정계 v1.0");
 			System.out.println("==============================");
