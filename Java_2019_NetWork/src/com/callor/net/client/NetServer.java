e1e6101e6559d5999db2a68041690eeb956aa0d1
callor <callor@callor.com>
2019-10-01 22:06:31 +0900

    network

--- Java_2019_NetWork/src/com/callor/net/client/NetServer.java
+++ Java_2019_NetWork/src/com/callor/net/client/NetServer.java
@@ -1,10 +1,41 @@
 package com.callor.net.client;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class NetServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		int port = 6974;
+        ServerSocket server = new ServerSocket(port);
+        System.out.println("Server is ready on " + port + "port.");
 
+        Scanner scan = new Scanner(System.in);
+        String data = new String();
+
+        System.out.println("Waiting connection...");
+        Socket client = server.accept();
+        System.out.println("Connected with " + client.getInetAddress() + ":" + client.getLocalPort());
+
+        OutputStream os = client.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
+
+        while((data = scan.nextLine()) != null )
+        {
+            if(data.equals("exit")) {
+                dos.writeUTF("exit");
+                break;
+            }
+
+            dos.writeUTF(data);
+            System.out.println("Send a message to client");
+        }
+        dos.close();
+        client.close();
 	}
 
 }
