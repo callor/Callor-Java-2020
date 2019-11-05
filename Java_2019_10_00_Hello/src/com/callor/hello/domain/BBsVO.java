c1cb79926c2befd827bf99dc9c2776c671a5a3eb
callor <callor@callor.com>
2019-11-05 17:25:57 +0900

    gg

---
+++ Java_2019_10_00_Hello/src/com/callor/hello/domain/BBsVO.java
@@ -0,0 +1,36 @@
+package com.callor.hello.domain;
+
+public class BBsVO {
+
+	private String num;
+	private String title;
+	private String auth;
+	private String text;
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuth() {
+		return auth;
+	}
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+}
