71fe2b6786bf1db82e27a46449ec48b91982fe96
callor <callor@callor.com>
2020-06-17 18:01:34 +0900

    homework

---
+++ Java_2020_90_HomeWork/src/com/callor/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.callor.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println(30 + 40);
+		System.out.println("대한민국만세");
+		System.out.println("Republic of Korea");
+		
+	}
+	
+	
+}
