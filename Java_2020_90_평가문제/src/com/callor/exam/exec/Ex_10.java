84013d68e0a42666dd8d8a863c7b530dc00f2fc4
callor <callor@callor.com>
2020-07-16 21:18:33 +0900

    평가문제

---
+++ Java_2020_90_평가문제/src/com/callor/exam/exec/Ex_10.java
@@ -0,0 +1,15 @@
+package com.callor.exam.exec;
+
+public class Ex_10 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 0; // 인터져
+		Integer num2 = 0;
+		
+		char c = 'A'; // 캐릭터
+		Character c1 = 'B';
+		
+	}
+
+}
---
+++ Java_2020_90_평가문제/src/com/callor/exam/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.callor.exam.service;
+
+public interface StudentService {
+
+	public void inputStudent();
+	public void studentList();
+	
+}
