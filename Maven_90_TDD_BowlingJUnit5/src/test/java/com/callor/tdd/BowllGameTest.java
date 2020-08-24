4b08767072b7edb70055e7adb588acc908d57d26
callor <callor@callor.com>
2020-08-24 13:28:33 +0900

    bowl

--- Maven_90_TDD_BowlingJUnit5/src/test/java/com/callor/tdd/BowllGameTest.java
+++ Maven_90_TDD_BowlingJUnit5/src/test/java/com/callor/tdd/BowllGameTest.java
@@ -2,8 +2,6 @@ package com.callor.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Random;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
