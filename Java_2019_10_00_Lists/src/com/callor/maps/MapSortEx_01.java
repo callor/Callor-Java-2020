db36d521690556351f230f1acf0c2c4bb5e09fd0
callor <callor@callor.com>
2020-09-26 07:55:48 +0900

    M

--- Java_2019_10_00_Lists/src/com/callor/maps/MapSortEx_01.java
+++ Java_2019_10_00_Lists/src/com/callor/maps/MapSortEx_01.java
@@ -3,7 +3,6 @@ package com.callor.maps;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
--- Java_2019_10_00_Lists/src/com/callor/maps/domain/MapsVO.java
+++ Java_2019_10_00_Lists/src/com/callor/maps/domain/MapsVO.java
@@ -1,8 +1,8 @@
 package com.callor.maps.domain;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
