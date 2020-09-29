fb71878f72e0fea03bef35f77cc21839e4b7a7f0
callor <callor@callor.com>
2020-09-29 11:01:08 +0900

    prime

---
+++ Java_2020_00_Hello/src/com/callor/alorithm/PrimeEx_01.java
@@ -0,0 +1,27 @@
+package com.callor.alorithm;
+
+import java.util.Random;
+
+public class PrimeEx_01 {
+
+	public static void main(String[] args) {
+
+		
+		Random rnd = new Random();
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			int num = rnd.nextInt(100) + 2;
+			int j = 0;
+			for(j = 2 ; j < num ; j++) {
+				if(num % j == 0) break;
+			}
+
+			if(j >= num) {
+				System.out.printf("%d 는 소수입니다\n",num);
+			} else {
+				System.out.printf("%d 는 소수가 아닙니다\n",num);
+			}
+		}
+	}
+
+}
