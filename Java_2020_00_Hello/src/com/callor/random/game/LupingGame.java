4e5cc9556c230eeaf6b2c353767e8c07148e2c78
callor <callor@callor.com>
2021-02-10 18:06:00 +0900

    LupingGame

---
+++ Java_2020_00_Hello/src/com/callor/random/game/LupingGame.java
@@ -0,0 +1,26 @@
+package com.callor.random.game;
+
+import java.util.Random;
+
+/*
+ * Random Class를 사용하여 1 ~ 100까지의 임의의 정수를 만들고
+ * 키보드로 정수를 입력받아 수를 맞추는 게임
+ * 
+ * 입력한값(inputNum)와 Random수(rndNum)와 비교하여
+ * rndNum > inputNum : 작은값 입력
+ * rndNum < inputNum : 큰값 입력
+ * rndNum == inputNum : 맞았어요
+ * 
+ * 반복문으로 실행
+ * 
+ */
+public class LupingGame {
+
+	public static void main(String[] args) {
+
+		Random rnd = new Random();
+		
+		
+	}
+
+}
