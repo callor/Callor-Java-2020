5e5115d863e3920e6ecf77e477013af0617a7547
callor <callor@callor.com>
2021-02-22 09:05:46 +0900

    wrapper

---
+++ Java_2020_00_Hello/src/com/callor/wrapperClass/IntegerEx.java
@@ -0,0 +1,20 @@
+package com.callor.wrapperClass;
+
+public class IntegerEx {
+	
+	
+	public static void main(String[] args) {
+		
+		Integer num = 30;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(num.doubleValue());
+		
+		Number number1 = 200;
+		Number number2 = 100;
+		System.out.println(number1.intValue() + number2.intValue());
+		
+		
+	}
+
+}
