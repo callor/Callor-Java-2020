ba18531e5a6f3fffd55d7efd82b5c5ef2cde13c8
callor <callor@callor.com>
2021-03-17 12:51:44 +0900

    3Oper

---
+++ Java_2020_00_Hello/src/com/callor/varriable/Operators.java
@@ -0,0 +1,16 @@
+package com.callor.varriable;
+
+public class Operators {
+	
+	public static void main(String[] args) {
+		
+		System.out.println( 3 == 3 ? "맞아" : "아니");
+		System.out.println( 3 % 2 == 0);
+		System.out.println( 4 % 2 == 0);
+		System.out.println( 5 % 2 == 0);
+		System.out.println( 5 % 2 == 0 ? "짝수" : "홀수");
+		
+		
+	}
+
+}
