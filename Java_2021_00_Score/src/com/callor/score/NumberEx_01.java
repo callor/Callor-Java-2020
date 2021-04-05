b6dc866531670be85c5d6937cf4ad33950f87eee
callor <callor@callor.com>
2021-04-05 15:09:22 +0900

    Deploy

--- Java_2021_00_Score/src/com/callor/score/NumberEx_01.java
+++ Java_2021_00_Score/src/com/callor/score/NumberEx_01.java
@@ -3,12 +3,9 @@ package com.callor.score;
 import com.callor.score.service.NumberServiceV1;
 
 public class NumberEx_01 {
-	
 	public static void main(String[] args) {
 		NumberServiceV1 nsV1 = new NumberServiceV1();
 		Integer retNum = nsV1.inputNum("새우깡");
 		System.out.println("새우깡 가격 : " + retNum);
 	}
-	
-
 }
