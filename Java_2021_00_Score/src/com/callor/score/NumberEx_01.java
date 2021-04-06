61b8eb4ba3c2ea8e9183bf19f5c8ce546078e4ea
callor <callor@callor.com>
2021-04-06 14:15:45 +0900

    NumberService

--- Java_2021_00_Score/src/com/callor/score/NumberEx_01.java
+++ Java_2021_00_Score/src/com/callor/score/NumberEx_01.java
@@ -1,11 +1,11 @@
 package com.callor.score;
 
-import com.callor.score.service.NumberServiceV1;
+import com.callor.score.service.impl.InputServiceImplV1;
 
 public class NumberEx_01 {
 	public static void main(String[] args) {
-		NumberServiceV1 nsV1 = new NumberServiceV1();
-		Integer retNum = nsV1.inputNum("새우깡");
+		InputServiceImplV1 nsV1 = new InputServiceImplV1();
+		Integer retNum = nsV1.inputValue("새우깡");
 		System.out.println("새우깡 가격 : " + retNum);
 	}
 }
---
+++ Java_2021_00_Score/src/com/callor/score/service/InputService.java
@@ -0,0 +1,11 @@
+package com.callor.score.service;
+
+public interface InputService {
+	
+	
+	public Integer inputValue();
+	public Integer inputValue(String title);
+	public Integer inputValue(String title,int start, int end);
+	
+
+}
