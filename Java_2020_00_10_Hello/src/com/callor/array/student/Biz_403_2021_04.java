5cd1f52d90c9a72222bf6b2787d68da197ce167e
callor <callor@callor.com>
2021-04-13 14:05:52 +0900

    Team

--- Java_2020_00_10_Hello/src/com/callor/array/student/Biz_403_2021_04.java
+++ Java_2020_00_10_Hello/src/com/callor/array/student/Biz_403_2021_04.java
@@ -7,8 +7,7 @@ public class Biz_403_2021_04 {
 	public static void main(String[] args) {
 
 		String[] strNames = new String[] { 
-				"김민식","김성훈","박성래","방영두","신어진",
-				"이민주","이성민","이창준","정류운","조혜승" 
+				"김민식","신어진","이민주","이성민","정류운" 
 				};
 
 		Random rnd = new Random();
@@ -30,7 +29,9 @@ public class Biz_403_2021_04 {
 		System.out.println("=".repeat(50));
 		System.out.println("노소연\t고은결\t김영진\t나나\t박은빈");
 		System.out.println("조아영\t김소정\t이유빈\t장혜미\t최선영");
-		System.out.println("최건진\t김기수\t기경현\t오종관\t조우석");
+		System.out.println("최건진\t김기수\t기경헌\t오종관\t조우석");
+		System.out.println("김성훈\t이창준\t방영두\t박성래\t조혜승");
+		System.out.println("t신어진\t김민식\t이민주\t이성민\t정류운");
 		System.out.println("=".repeat(50));
 		
 		for (int i = 0; i < strNames.length; i++) {
